bee888881cc406ee93decd086e4b403134a55c10
Garik <garfild_2003@mail.ru>
2023-10-25 19:09:25 +0300

    Included application

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from .views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls'))
 ]
