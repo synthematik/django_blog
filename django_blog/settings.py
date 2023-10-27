35331a10e1aa9bd466ff3185882424c316c7c5a7
Garik <garfild_2003@mail.ru>
2023-10-27 19:56:28 +0300

    Connected database

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_post',
+        'USER': 'postgres',
+        'PASSWORD': 'garik',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
