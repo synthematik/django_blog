f831068f2d5e518e2b61cf8797ee6c7241116e44
Garik <garfild_2003@mail.ru>
2023-10-27 20:04:21 +0300

    Added patterns for Post and Tag pades

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', posts_list)
+    path('', posts_list, name='post_list_url'),
+    path('post/<str:slug>/', post_detail, name='post_detail_url'),
+    path('tags/', tags_list, name='tags_list_url'),
+    path('tag/<str:slug>', tag_detail, name='tag_detail_url')
 ]
