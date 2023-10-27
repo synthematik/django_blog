1c874d5951744018bb1ea950c5bd96acb685a84e
Garik <garfild_2003@mail.ru>
2023-10-27 20:04:57 +0300

    Added functions for Post and Tag pades

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post, Tag
 
 def posts_list(request):
-    return HttpResponse('<h1>Hello</h1>')
+    posts = Post.objects.all()
+    return render(request,'blog/base_blog.html', context={'posts':posts})
+
+def post_detail(request, slug):
+    posts = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/post_detail.html', context={'posts':posts})
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags})
+
+def tag_detail(request, slug):
+    tag = Tag.objects.get(slug__iexact=slug)
+    return render(request, 'blog/tag_detail.html', context={'tag': tag})
