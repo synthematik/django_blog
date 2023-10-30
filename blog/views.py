01833dd0a018e6dcb22562f8edef6fb44e029e81
Garik <garfild_2003@mail.ru>
2023-10-30 22:55:38 +0300

    Added Class Based Views

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import View
 from .models import Post, Tag
+from .utils import ObjectDetailMixin
+
 
 def posts_list(request):
     posts = Post.objects.all()
     return render(request,'blog/base_blog.html', context={'posts':posts})
-
-def post_detail(request, slug):
-    posts = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'posts':posts})
 
 def tags_list(request):
     tags = Tag.objects.all()
@@ -16,3 +15,13 @@ def tags_list(request):
 def tag_detail(request, slug):
     tag = Tag.objects.get(slug__iexact=slug)
     return render(request, 'blog/tag_detail.html', context={'tag': tag})
+
+
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
