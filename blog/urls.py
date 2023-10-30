5526cf884e51c9b1694eb9e9c11c68a67ae896ac
Garik <garfild_2003@mail.ru>
2023-10-30 22:55:00 +0300

    Updated (with new views)

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', posts_list, name='post_list_url'),
-    path('post/<str:slug>/', post_detail, name='post_detail_url'),
+    path('post/<str:slug>/', PostDetail.as_view(), name='post_detail_url'),
     path('tags/', tags_list, name='tags_list_url'),
-    path('tag/<str:slug>', tag_detail, name='tag_detail_url')
+    path('tag/<str:slug>', TagDetail.as_view(), name='tag_detail_url'),
 ]
