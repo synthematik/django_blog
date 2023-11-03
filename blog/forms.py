cf1902db7611f2f9f93ab2e0a17e7c6490e2afac
Garik <garfild_2003@mail.ru>
2023-11-03 20:41:48 +0300

    Data validation added

--- blog/forms.py
+++ blog/forms.py
@@ -1,14 +1,44 @@
 from django import forms
 from .models import Tag, Post
+from django.core.exceptions import ValidationError
 
 
-class TagForm(forms.Form):
-    title = forms.CharField(max_length=50)
-    slug = forms.CharField(max_length=50)
+class TagForm(forms.ModelForm):
 
-    def save(self):
-        new_tag = Tag.objects.create(
-            title=self.cleaned_data['title'],
-            slug=self.cleaned_data['slug']
-        )
-        return new_tag
+    class Meta:
+        model = Tag
+        fields = ['title', 'slug']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'})
+        }
+    def clean_slug(self):
+        new_slug = self.cleaned_data['slug'].lower()
+
+        if new_slug == 'create':
+            raise ValidationError('Не может быть создан')
+        if Tag.objects.filter(slug__iexact=new_slug).count():
+            raise ValidationError('Такой слаг уже существует')
+
+        return new_slug
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'slug', 'body', 'tags']
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'slug': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+            'tags': forms.SelectMultiple(attrs={'class': 'form-control'})
+        }
+
+        def clean_slug(self):
+            new_slug = self.cleaned_data['slug'].lower()
+
+            if new_slug == 'create':
+                raise ValidationError('Не может быть создан')
+            return new_slug
+
