8c9e4702e5919ac6d0ba7505f2c750d05832dc24
Garik <garfild_2003@mail.ru>
2023-11-03 20:38:33 +0300

    Added mixin for creating posts and tags

--- blog/utils.py
+++ blog/utils.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 
 class ObjectDetailMixin:
@@ -7,3 +7,22 @@ class ObjectDetailMixin:
     def get(self, request, slug):
         obj = get_object_or_404(self.model, slug__iexact=slug)
         return render(request, self.template, context={self.model.__name__.lower(): obj})
+
+
+class ObjectCreateMixin:
+    form_model = None
+    template = None
+
+
+    def get(self, request):
+        form = self.form_model()
+        return render(request, self.template, context={'form': form})
+
+    def post(self, request):
+        bound_form = self.form_model(request.POST)
+
+        if bound_form.is_valid():
+            new_obj = bound_form.save()
+            return redirect(new_obj)
+
+        return render(request, self.template, context={'form': bound_form})
