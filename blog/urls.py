fb0ba1bfe11867701c5bb8523dfb06e77b7f59a1
Garik <garfild_2003@mail.ru>
2023-11-03 20:37:25 +0300

    Added paths for creating posts and tags

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('', posts_list, name='post_list_url'),
+    path('post/create/', PostCreate.as_view(), name='post_create_url'),
     path('post/<str:slug>/', PostDetail.as_view(), name='post_detail_url'),
     path('tags/', tags_list, name='tags_list_url'),
-    path('tag/<str:slug>', TagDetail.as_view(), name='tag_detail_url'),
+    path('tag/create/', TagCreate.as_view(), name = 'tag_create_url'),
+    path('tag/<str:slug>/', TagDetail.as_view(), name='tag_detail_url')
 ]
