37118116861e7ef62fe708e8ff8d68751588426c
Garik <garfild_2003@mail.ru>
2023-11-04 22:19:33 +0300

    Added paths for updating posts and tags

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,9 @@ urlpatterns = [
     path('', posts_list, name='post_list_url'),
     path('post/create/', PostCreate.as_view(), name='post_create_url'),
     path('post/<str:slug>/', PostDetail.as_view(), name='post_detail_url'),
+    path('post/<str:slug>/update/', PostUpdate.as_view(), name='post_update_url'),
     path('tags/', tags_list, name='tags_list_url'),
     path('tag/create/', TagCreate.as_view(), name = 'tag_create_url'),
-    path('tag/<str:slug>/', TagDetail.as_view(), name='tag_detail_url')
+    path('tag/<str:slug>/', TagDetail.as_view(), name='tag_detail_url'),
+    path('tag/<str:slug>/update/', TagUpdate.as_view(), name='tag_update_url')
 ]
