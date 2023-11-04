397f4f7c2593b72ccf6a0b6bb98a8941f8465e76
Garik <garfild_2003@mail.ru>
2023-11-04 22:17:23 +0300

    Added new function

--- blog/models.py
+++ blog/models.py
@@ -19,6 +19,9 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('post_detail_url', kwargs={'slug': self.slug})
 
+    def get_update_url(self):
+        return reverse('post_update_url', kwargs={'slug': self.slug})
+
     def save(self, *args, **kwargs):
         if not self.id:
             self.slug = gen_slug(self.title)
@@ -34,6 +37,9 @@ class Tag(models.Model):
 
     def get_absolute_url(self):
         return reverse('tag_detail_url', kwargs={'slug': self.slug})
+
+    def get_update_url(self):
+        return reverse('tag_update_url', kwargs={'slug':self.slug})
 
     def __str__(self):
         return (self.title)
