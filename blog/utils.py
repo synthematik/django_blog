b769d33a6b777ce1eb18688c6f76e8117d79d523
Garik <garfild_2003@mail.ru>
2023-11-04 22:16:03 +0300

    Added mixins for updating posts and tags

--- blog/utils.py
+++ blog/utils.py
@@ -26,3 +26,24 @@ class ObjectCreateMixin:
             return redirect(new_obj)
 
         return render(request, self.template, context={'form': bound_form})
+
+
+class ObjectUpdateMixin:
+    model = None
+    model_form = None
+    template = None
+
+    def get(self, request, slug):
+        obj = self.model.objects.get(slug__iexact=slug)
+        bound_form = self.model_form(instance=obj)
+        return render(request, self.template, context={'form': bound_form, self.model.__name__.lower(): obj})
+
+    def post(self, request, slug):
+        obj = self.model.objects.get(slug__iexact=slug)
+        bound_form = self.model_form(request.POST, instance=obj)
+
+        if bound_form.is_valid():
+            new_obj = bound_form.save()
+            return redirect(new_obj)
+
+        return render(request, self.template, context={'form': bound_form, self.model.__name__.lower(): obj})
