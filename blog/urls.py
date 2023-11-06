d43905bb1d8e7e756158d84d9758743de2c6d4ec
Garik <garfild_2003@mail.ru>
2023-11-06 20:21:55 +0300

    Added path to delete posts and tags

--- blog/urls.py
+++ blog/urls.py
@@ -6,8 +6,11 @@ urlpatterns = [
     path('post/create/', PostCreate.as_view(), name='post_create_url'),
     path('post/<str:slug>/', PostDetail.as_view(), name='post_detail_url'),
     path('post/<str:slug>/update/', PostUpdate.as_view(), name='post_update_url'),
+    path('post/<str:slug>/delete/', PostDelete.as_view(), name='post_delete_url'),
     path('tags/', tags_list, name='tags_list_url'),
     path('tag/create/', TagCreate.as_view(), name = 'tag_create_url'),
     path('tag/<str:slug>/', TagDetail.as_view(), name='tag_detail_url'),
-    path('tag/<str:slug>/update/', TagUpdate.as_view(), name='tag_update_url')
+    path('tag/<str:slug>/update/', TagUpdate.as_view(), name='tag_update_url'),
+    path('tag/<str:slug>/delete/', TagDelete.as_view(), name='tag_delete_url')
+
 ]
