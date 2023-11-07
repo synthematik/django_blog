4a9fc027bc099919c945435cfeeff224aedfffac
Garik <garfild_2003@mail.ru>
2023-11-07 18:30:14 +0300

    Small improvement for the correct operation of the admin panel

--- blog/utils.py
+++ blog/utils.py
@@ -5,15 +5,15 @@ from django.urls import reverse
 class ObjectDetailMixin:
     model = None
     template = None
+
     def get(self, request, slug):
         obj = get_object_or_404(self.model, slug__iexact=slug)
-        return render(request, self.template, context={self.model.__name__.lower(): obj})
+        return render(request, self.template, context={self.model.__name__.lower(): obj, 'admin_obj': obj, 'detail': True})
 
 
 class ObjectCreateMixin:
     form_model = None
     template = None
-
 
     def get(self, request):
         form = self.form_model()
