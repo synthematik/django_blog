23f1e4516b46b65390a1cd853de7398d4f103f4e
Garik <garfild_2003@mail.ru>
2023-11-10 19:19:14 +0300

    Connected models to the admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post, Tag
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date_pub')
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['title']
