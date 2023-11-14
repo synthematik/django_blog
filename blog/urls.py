d256df36a146f37123dc4c9d31e5b523ad231ac3
Garik <garfild_2003@mail.ru>
2023-11-14 14:14:43 +0300

    Added path for creating comment

--- blog/urls.py
+++ blog/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
     path('post/<str:slug>/', PostDetail.as_view(), name='post_detail_url'),
     path('post/<str:slug>/update/', PostUpdate.as_view(), name='post_update_url'),
     path('post/<str:slug>/delete/', PostDelete.as_view(), name='post_delete_url'),
+    path('post/<str:slug>/comment/', CommentCreate.as_view(), name='comment_create_url'),
     path('tags/', tags_list, name='tags_list_url'),
     path('tag/create/', TagCreate.as_view(), name = 'tag_create_url'),
     path('tag/<str:slug>/', TagDetail.as_view(), name='tag_detail_url'),
     path('tag/<str:slug>/update/', TagUpdate.as_view(), name='tag_update_url'),
     path('tag/<str:slug>/delete/', TagDelete.as_view(), name='tag_delete_url')
-
 ]
