447bdef3f7cf9ede4df52f6298447a08860539dc
Garik <garfild_2003@mail.ru>
2023-11-14 20:27:43 +0300

    Added a simple search by body and title of posts

--- blog/views.py
+++ blog/views.py
@@ -6,10 +6,15 @@ from .forms import TagForm, PostForm, CommentForm
 from django.urls import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 
 def posts_list(request):
-    posts = Post.objects.all()
+    search_query = request.GET.get('search', '')
+    if search_query:
+        posts = Post.objects.filter(Q(title__icontains=search_query) | Q(body__icontains=search_query))
+    else:
+        posts = Post.objects.all()
 
     paginator = Paginator(posts,2)
     page_num = request.GET.get('page', 1)
